d8e7309e5540e7991278f87e075bc827667aaca0
Chris Guida <chrisguida@gmail.com>
2023-04-21 15:08:25 -0600

    update to new custom bkpr tags, watchdescriptor hangs even though handler completes

--- src/main.rs
+++ src/main.rs
@@ -6,6 +6,8 @@ extern crate serde_json;
 
 use std::fmt::Debug;
 use std::sync::Arc;
+use std::thread::{self, sleep};
+use std::time;
 use tokio::sync::Mutex;
 
 use anyhow::Ok;
@@ -20,8 +22,8 @@ use bdk::electrum_client::Client;
 use bdk::{bitcoin, descriptor, Balance, SyncOptions, TransactionDetails, Wallet};
 use watchdescriptor::state::WatchDescriptor;
 
-const COIN_DEPOSIT_TAG: &str = "coin_onchain_deposit";
-const COIN_SPEND_TAG: &str = "coin_onchain_spend";
+const UTXO_DEPOSIT_TAG: &str = "utxo_deposit";
+const UTXO_SPENT_TAG: &str = "utxo_spent";
 
 #[tokio::main]
 async fn main() -> Result<(), anyhow::Error> {
@@ -36,8 +38,8 @@ async fn main() -> Result<(), anyhow::Error> {
             options::Value::String(bitcoin::Network::Bitcoin.to_string()),
             "Which network to use: [bitcoin, testnet, signet, regtest]",
         ))
-        .notification(messages::NotificationTopic::new(COIN_DEPOSIT_TAG))
-        .notification(messages::NotificationTopic::new(COIN_SPEND_TAG))
+        .notification(messages::NotificationTopic::new(UTXO_DEPOSIT_TAG))
+        .notification(messages::NotificationTopic::new(UTXO_SPENT_TAG))
         .rpcmethod(
             "watchdescriptor",
             "Watch one or more external wallet descriptors and emit notifications when coins are moved",
@@ -64,20 +66,20 @@ async fn send_spend_notification(
     tx: &TransactionDetails,
 ) -> Result<(), Error> {
     let acct = "external";
-    let transfer_from: Option<String> = None;
     let amount = 1000;
     let outpoint = "a18b557b03f2b2d0e25430ef75b70ff5b6bd1f4dd19da3a564502b92623cd8a5:0";
-    let onchain_deposit = json!({
+    let txid = "b61d6cd9236bc6cd1ebc38bb3e82d2aeea3502a85feaa7987c63ea9e48537dd5";
+    let onchain_spend = json!({
         "account": acct,
-        "transfer_from": transfer_from,
         "outpoint": outpoint,
+        "spending_txid": txid,
         "amount_msat": amount,
         "coin_type": "bcrt",
         "timestamp": 1679955976,
         "blockheight": 111,
     });
     plugin
-        .send_custom_notification(COIN_SPEND_TAG.to_string(), onchain_deposit)
+        .send_custom_notification(UTXO_SPENT_TAG.to_string(), onchain_spend)
         .await?;
     Ok(())
 }
@@ -91,17 +93,24 @@ async fn send_deposit_notification(
     let amount = 1000;
     let outpoint = "a18b557b03f2b2d0e25430ef75b70ff5b6bd1f4dd19da3a564502b92623cd8a5:0";
     let onchain_deposit = json!({
-        "account": acct,
-        "transfer_from": transfer_from,
-        "outpoint": outpoint,
-        "amount_msat": amount,
-        "coin_type": "bcrt",
-        "timestamp": 1679955976,
-        "blockheight": 111,
+            "account": acct,
+            "transfer_from": transfer_from,
+            "outpoint": outpoint,
+            "amount_msat": amount,
+            "coin_type": "bcrt",
+            "timestamp": 1679955976,
+            "blockheight": 111,
     });
-    plugin
-        .send_custom_notification(COIN_DEPOSIT_TAG.to_string(), onchain_deposit)
-        .await?;
+    log::info!("INSIDE SEND DEPOSIT NOTIFICATION ON WATCHDESCRIPTOR SIDE");
+    let cloned_plugin = plugin.clone();
+    tokio::spawn(async move {
+        if let Err(e) = cloned_plugin
+            .send_custom_notification(UTXO_DEPOSIT_TAG.to_string(), onchain_deposit)
+            .await
+        {
+            log::error!("Error sending custom notification: {:?}", e);
+        }
+    });
     Ok(())
 }
 
@@ -111,10 +120,12 @@ async fn send_notifications_for_tx(
 ) -> Result<(), Error> {
     log::info!("sending notifs for tx: {:?}", tx);
     if tx.sent > 0 {
+        log::info!("sending spend notif");
         send_spend_notification(plugin, &tx).await?;
     }
 
     if tx.received > 0 {
+        log::info!("sending deposit notif");
         send_deposit_notification(plugin, &tx).await?;
     }
     Ok(())
@@ -174,17 +185,23 @@ async fn watchdescriptor(
         if new_txs.len() > 0 {
             for tx in new_txs {
                 log::info!("new tx found!: {:?}", tx);
+                send_notifications_for_tx(&plugin, tx).await?;
             }
         } else {
             log::info!("no new txs this time");
         }
     }
+    log::info!("waiting for wallet lock");
     plugin.state().lock().await.add_descriptor_wallet(dw);
+    log::info!("wallet added");
     let messasge = format!(
         // "Wallet with checksum {} successfully added",
         "Wallet successfully added",
         // wallet.descriptor_checksum(bdk::KeychainKind::External)
     );
+    let ten_millis = time::Duration::from_secs(30);
+    thread::sleep(ten_millis);
+    log::info!("returning");
     Ok(json!(messasge))
 }
 
@@ -214,12 +231,14 @@ async fn block_added_handler(plugin: Plugin<State>, v: serde_json::Value) -> Res
     let blockchain = ElectrumBlockchain::from(client);
     let mut transactions = Vec::<TransactionDetails>::new();
     // for dw in descriptor_wallets.iter_mut() {
-    for dw in plugin.state().lock().await.wallets.iter_mut() {
+    // for dw in plugin.state().lock().await.wallets.iter_mut() {
+    for i in 0..1 {
         {
             let wallet = Wallet::new(
-                &dw.descriptor,
-                dw.change_descriptor.as_ref(),
-                // "wpkh(tpubEBr4i6yk5nf5DAaJpsi9N2pPYBeJ7fZ5Z9rmN4977iYLCGco1VyjB9tvvuvYtfZzjD5A8igzgw3HeWeeKFmanHYqksqZXYXGsw5zjnj7KM9/*)",
+                // &dw.descriptor,
+                // dw.change_descriptor.as_ref(),
+                "wpkh(tpubEBr4i6yk5nf5DAaJpsi9N2pPYBeJ7fZ5Z9rmN4977iYLCGco1VyjB9tvvuvYtfZzjD5A8igzgw3HeWeeKFmanHYqksqZXYXGsw5zjnj7KM9/*)",
+                None,
                 // bitcoin::Network::Bitcoin,
                 bitcoin::Network::Testnet,
                 MemoryDatabase::default(),
@@ -240,24 +259,27 @@ async fn block_added_handler(plugin: Plugin<State>, v: serde_json::Value) -> Res
             // } else {
             //     log::info!("no new txs this time");
             // }
+        } else {
+            log::info!("found no transactions");
         }
     }
 
-    // let acct = "test account";
-    // let transfer_from: Option<String> = None;
-    // let amount = 1000;
-    // let outpoint = "a18b557b03f2b2d0e25430ef75b70ff5b6bd1f4dd19da3a564502b92623cd8a5:0";
-    // let onchain_deposit = json!({
-    //     "account": acct,
-    //     "transfer_from": transfer_from,
-    //     "outpoint": outpoint,
-    //     "amount_msat": amount,
-    //     "coin_type": "bcrt",
-    //     "timestamp": 1679955976,
-    //     "blockheight": 111,
-    // });
-    // plugin
-    //     .send_custom_notification(COIN_DEPOSIT_TAG.to_string(), onchain_deposit)
-    //     .await?;
+    let acct = "test account";
+    let transfer_from: Option<String> = None;
+    let amount = 1000;
+    let outpoint = "a18b557b03f2b2d0e25430ef75b70ff5b6bd1f4dd19da3a564502b92623cd8a5:0";
+    let onchain_deposit = json!({
+        "account": acct,
+        "transfer_from": transfer_from,
+        "outpoint": outpoint,
+        "amount_msat": amount,
+        "coin_type": "bcrt",
+        "timestamp": 1679955976,
+        "blockheight": 111,
+    });
+    log::info!("sending test notification for SANITY");
+    plugin
+        .send_custom_notification(UTXO_DEPOSIT_TAG.to_string(), onchain_deposit)
+        .await?;
     Ok(())
 }
