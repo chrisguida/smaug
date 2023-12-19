004401d1a73bce71dcb59a55eeedbe31f678c451
fmhoeger <fmhoeger@mirsem.org>
2023-12-18 18:47:34 -0600

    Add test for RPC list

--- tests/test_rpc.py
+++ tests/test_rpc.py
@@ -42,3 +42,43 @@ def test_rpc_remove(node_factory, bitcoind):
     assert len(smaug_wallets) == 0
     assert result == f"Deleted wallet: {wallet_name}"
     assert not os.path.isfile(db_path)
+
+
+def test_rpc_list(node_factory, bitcoind):
+    """
+    Test RPC list.
+    """
+
+    l1 = node_factory.get_nodes(
+        1,
+        opts={
+            "allow_broken_log": True,
+            "plugin": SMAUG_PLUGIN,
+            "smaug_brpc_user": BITCOIND_CONFIG["rpcuser"],
+            "smaug_brpc_pass": BITCOIND_CONFIG["rpcpassword"],
+            "smaug_brpc_port": BITCOIND_CONFIG["rpcport"],
+        },
+    )[0]
+
+    # get external/internal only_one descriptors
+    external_descriptor = get_only_one_descriptor(bitcoind, "wpkh", False)
+    internal_descriptor = get_only_one_descriptor(bitcoind, "wpkh", True)
+
+    external_descriptor_tr = get_only_one_descriptor(bitcoind, "tr", False)
+    internal_descriptor_tr = get_only_one_descriptor(bitcoind, "tr", True)
+
+    # add two wallets to smaug
+    wallet1 = l1.rpc.smaug("add", external_descriptor, internal_descriptor)
+    wallet2 = l1.rpc.smaug("add", external_descriptor_tr, internal_descriptor_tr)
+    wallet1_name = wallet1["name"]
+    wallet2_name = wallet2["name"]
+
+    smaug_wallets = l1.rpc.smaug("ls")
+    assert len(smaug_wallets) == 2
+    assert wallet1_name in smaug_wallets
+    assert wallet2_name in smaug_wallets
+
+    wallet1_cd = smaug_wallets[wallet1_name]["change_descriptor"]
+    wallet2_cd = smaug_wallets[wallet2_name]["change_descriptor"]
+    assert wallet1_cd.startswith("wpkh(")
+    assert wallet2_cd.startswith("tr(")
