d336396d791cb258b6d31a8fae46cad4c9251cc8
fmhoeger <fmhoeger@mirsem.org>
2023-12-27 18:57:31 -0600

    Add sudo

--- .github/scripts/install-cln.sh
+++ .github/scripts/install-cln.sh
@@ -4,4 +4,4 @@ set -e
 
 cd /tmp/
 wget "https://github.com/ElementsProject/lightning/releases/download/v${CLN_VERSION}/clightning-v${CLN_VERSION}-Ubuntu-22.04.tar.xz"
-tar -xvf clightning-v${CLN_VERSION}-Ubuntu-22.04.tar.xz -C /usr/local --strip-components=2
+sudo tar -xvf clightning-v${CLN_VERSION}-Ubuntu-22.04.tar.xz -C /usr/local --strip-components=2
