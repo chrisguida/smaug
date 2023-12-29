ccec3745e349f766c66548af0d689813592f40d3
fmhoeger <fmhoeger@mirsem.org>
2023-12-29 01:09:45 -0600

    Build CLN from source

--- .github/scripts/install-cln.sh
+++ .github/scripts/install-cln.sh
@@ -2,7 +2,22 @@
 
 set -e
 
-cd /tmp/
-wget "https://github.com/ElementsProject/lightning/releases/download/v${CLN_VERSION}/clightning-v${CLN_VERSION}-Ubuntu-22.04.tar.xz"
-tar -xvf clightning-v${CLN_VERSION}-Ubuntu-22.04.tar.xz --strip-components=2
-sudo mv ./bin/* /usr/local/bin
+sudo apt-get update
+sudo apt-get install -y \
+  autoconf automake build-essential git libtool libsqlite3-dev \
+  python3 python3-pip net-tools zlib1g-dev libsodium-dev gettext
+pip3 install --upgrade pip
+pip3 install --user poetry
+sudo apt-get install -y cargo rustfmt protobuf-compiler
+pip3 install --upgrade pip
+pip3 install mako
+
+git clone https://github.com/niftynei/lightning.git
+cd lightning
+git checkout 37ad798a02336a82460b865fd4e6a29d8880856c
+
+pip3 install -r plugins/clnrest/requirements.txt
+
+./configure
+make -j$(nproc)
+sudo make install
