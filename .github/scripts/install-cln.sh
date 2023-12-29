5f9802385531a1bc86a82b73edcc476c0d5a37af
fmhoeger <fmhoeger@mirsem.org>
2023-12-28 23:26:28 -0600

    Try fixing cln install script

--- .github/scripts/install-cln.sh
+++ .github/scripts/install-cln.sh
@@ -5,4 +5,4 @@ set -e
 cd /tmp/
 wget "https://github.com/ElementsProject/lightning/releases/download/v${CLN_VERSION}/clightning-v${CLN_VERSION}-Ubuntu-22.04.tar.xz"
 tar -xvf clightning-v${CLN_VERSION}-Ubuntu-22.04.tar.xz --strip-components=2
-sudo mv "usr/bin/* "/usr/local/bin"
+sudo mv "usr/bin/*" "/usr/local/bin"
