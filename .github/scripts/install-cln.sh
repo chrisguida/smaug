e6a968eb88261af87996133442b49e67b31b9810
fmhoeger <fmhoeger@mirsem.org>
2023-12-30 10:44:08 -0600

    Cleanup

--- .github/scripts/install-cln.sh
+++ .github/scripts/install-cln.sh
@@ -6,10 +6,8 @@ sudo apt-get update
 sudo apt-get install -y \
   autoconf automake build-essential git libtool libsqlite3-dev \
   python3 python3-pip net-tools zlib1g-dev libsodium-dev gettext
-pip3 install --upgrade pip
-pip3 install --user poetry
 sudo apt-get install -y cargo rustfmt protobuf-compiler
-pip3 install --upgrade pip
+
 pip3 install mako
 
 git clone https://github.com/niftynei/lightning.git
