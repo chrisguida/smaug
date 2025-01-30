687fb0e76fcb478435d398d8976075353abc8c6c
Chris Guida <chrisguida@gmail.com>
2025-01-29 21:52:50 -0600

    update poetry deps and fix tests

--- tests/conftest.py
+++ tests/conftest.py
@@ -81,51 +81,9 @@ def ln_node(node_factory):  # noqa: F811
     yield node_factory.get_nodes(
         1,
         opts={
-            "allow_broken_log": True,
             "plugin": SMAUG_PLUGIN,
             "smaug_brpc_user": BITCOIND_CONFIG["rpcuser"],
             "smaug_brpc_pass": BITCOIND_CONFIG["rpcpassword"],
             "smaug_brpc_port": BITCOIND_CONFIG["rpcport"],
         },
     )[0]
-
-
-# SMAUG_PLUGIN = Path("~/.cargo/bin/smaug").expanduser()
-
-
-# def write_toml_config(filename, opts):
-#     with open(filename, "w") as f:
-#         for k, v in opts.items():
-#             if isinstance(v, str):
-#                 f.write('{} = "{}"\n'.format(k, v))
-#             else:
-#                 f.write("{} = {}\n".format(k, v))
-
-# @pytest.hookimpl(tryfirst=True, hookwrapper=True)
-# def pytest_runtest_makereport(item, call):
-#     # execute all other hooks to obtain the report object
-#     outcome = yield
-#     rep = outcome.get_result()
-
-#     # set a report attribute for each phase of a call, which can
-#     # be "setup", "call", "teardown"
-
-#     setattr(item, "rep_" + rep.when, rep)
-
-
-# def pytest_configure(config):
-#     config.addinivalue_line(
-#         "markers",
-#         "developer: only run when developer is flagged on",
-#     )
-
-
-# def pytest_runtest_setup(item):
-#     for mark in item.iter_markers(name="developer"):
-#         pass
-
-
-# @pytest.fixture(scope="function", autouse=True)
-# def log_name(request):
-#     # Here logging is used, you can use whatever you want to use for logs
-#     logging.info("Starting '{}'".format(request.node.name))
