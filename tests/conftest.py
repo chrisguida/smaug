4e972692baa1425a259c5625182e99d151047131
Chris Guida <chrisguida@gmail.com>
2025-01-30 13:57:37 -0600

    clean up tests, add pull request trigger and setup script, dynamically fetch path to plugin binary

--- tests/conftest.py
+++ tests/conftest.py
@@ -3,29 +3,27 @@ import os
 from pathlib import Path
 
 import pytest
-from pyln.testing.fixtures import (  # noqa: F401
-    db_provider,
-    directory,
-    env,
-    executor,
-    jsonschemas,
-    network_daemons,
-    node_cls,
-    node_factory,
-    teardown_checks,
-    test_base_dir,
-    test_name,
-)
+from pyln.testing.fixtures import *  # noqa: F403
 from pyln.testing.utils import BITCOIND_CONFIG, write_config
 
-SMAUG_PLUGIN = Path.cwd().joinpath("target/debug/smaug")
+# Define utility paths
+RUST_PROFILE = os.environ.get("RUST_PROFILE", "debug")
+COMPILED_PATH = Path.cwd() / "target" / RUST_PROFILE / "smaug"
+DOWNLOAD_PATH = Path.cwd() / "tests" / "smaug"
 
 
-# copied from https://github.com/elementsproject/lightning/blob/37ad798a02336a82460b865fd4e6a29d8880856c/contrib/pyln-testing/pyln/testing/fixtures.py#L127-L164  # noqa: E501
-# hacked to include blockfilterindex
+def get_plugin():
+    if COMPILED_PATH.is_file():
+        return COMPILED_PATH
+    elif DOWNLOAD_PATH.is_file():
+        return DOWNLOAD_PATH
+    else:
+        raise ValueError("No plugin was found.")
+
+
 @pytest.fixture
 def bitcoind(directory, teardown_checks):  # noqa: F811
-    chaind = network_daemons[env("TEST_NETWORK", "regtest")]
+    chaind = network_daemons[env("TEST_NETWORK", "regtest")]  # noqa: F405
     bitcoind = chaind(bitcoin_dir=directory)
 
     BITCOIND_REGTEST = {"rpcport": BITCOIND_CONFIG["rpcport"]}
@@ -44,23 +42,23 @@ def bitcoind(directory, teardown_checks):  # noqa: F811
 
     info = bitcoind.rpc.getnetworkinfo()
 
-    # FIXME: include liquid-regtest in this check after elementsd has been
-    # updated
-    if info["version"] < 200100 and env("TEST_NETWORK") != "liquid-regtest":
+    if (
+        info["version"] < 200100
+        and env("TEST_NETWORK") != "liquid-regtest"  # noqa: F405
+    ):
         bitcoind.rpc.stop()
         raise ValueError(
-            "bitcoind is too old. At least version 20100 (v0.20.1)"
-            " is needed, current version is {}".format(info["version"])
+            "bitcoind is too old. At least version 20100 (v0.20.1) is "
+            f"needed, current version is {info['version']}"
         )
     elif info["version"] < 160000:
         bitcoind.rpc.stop()
         raise ValueError(
-            "elementsd is too old. At least version 160000 (v0.16.0)"
-            " is needed, current version is {}".format(info["version"])
+            "elementsd is too old. At least version 160000 (v0.16.0) is "
+            f"needed, current version is {info['version']}"
         )
 
     info = bitcoind.rpc.getblockchaininfo()
-    # Make sure we have some spendable funds
     if info["blocks"] < 101:
         bitcoind.generate_block(101 - info["blocks"])
     elif bitcoind.rpc.getwalletinfo()["balance"] < 1:
@@ -81,50 +79,9 @@ def ln_node(node_factory):  # noqa: F811
     yield node_factory.get_nodes(
         1,
         opts={
-            "plugin": SMAUG_PLUGIN,
+            "plugin": get_plugin(),
             "smaug_brpc_user": BITCOIND_CONFIG["rpcuser"],
             "smaug_brpc_pass": BITCOIND_CONFIG["rpcpassword"],
             "smaug_brpc_port": BITCOIND_CONFIG["rpcport"],
         },
     )[0]
-
-
-# SMAUG_PLUGIN = Path("~/.cargo/bin/smaug").expanduser()
-
-
-# def write_toml_config(filename, opts):
-#     with open(filename, "w") as f:
-#         for k, v in opts.items():
-#             if isinstance(v, str):
-#                 f.write('{} = "{}"\n'.format(k, v))
-#             else:
-#                 f.write("{} = {}\n".format(k, v))
-
-# @pytest.hookimpl(tryfirst=True, hookwrapper=True)
-# def pytest_runtest_makereport(item, call):
-#     # execute all other hooks to obtain the report object
-#     outcome = yield
-#     rep = outcome.get_result()
-
-#     # set a report attribute for each phase of a call, which can
-#     # be "setup", "call", "teardown"
-
-#     setattr(item, "rep_" + rep.when, rep)
-
-
-# def pytest_configure(config):
-#     config.addinivalue_line(
-#         "markers",
-#         "developer: only run when developer is flagged on",
-#     )
-
-
-# def pytest_runtest_setup(item):
-#     for mark in item.iter_markers(name="developer"):
-#         pass
-
-
-# @pytest.fixture(scope="function", autouse=True)
-# def log_name(request):
-#     # Here logging is used, you can use whatever you want to use for logs
-#     logging.info("Starting '{}'".format(request.node.name))
---
+++ tests/setup.sh
@@ -0,0 +1,69 @@
+#!/bin/bash
+set -x
+# Get the directory of the script
+script_dir=$(dirname -- "$(readlink -f -- "$0")")
+
+cargo_toml_path="$script_dir/../Cargo.toml"
+
+# Use grep and awk to extract the name and version
+name=$(awk -F'=' '/^\[package\]/ { in_package = 1 } in_package && /name/ { gsub(/[" ]/, "", $2); print $2; exit }' "$cargo_toml_path")
+version=$(awk -F'=' '/^\[package\]/ { in_package = 1 } in_package && /version/ { gsub(/[" ]/, "", $2); print $2; exit }' "$cargo_toml_path")
+
+get_platform_file_end() {
+    machine=$(uname -m)
+    kernel=$(uname -s)
+
+    case $kernel in
+        Darwin)
+            echo 'universal-apple-darwin.zip'
+            ;;
+        Linux)
+            case $machine in
+                x86_64)
+                    echo 'x86_64-linux-gnu.tar.gz'
+                    ;;
+                armv7l)
+                    echo 'armv7-linux-gnueabihf.tar.gz'
+                    ;;
+                aarch64)
+                    echo 'aarch64-linux-gnu.tar.gz'
+                    ;;
+                *)
+                    echo "No self-compiled binary found and unsupported release-architecture: $machine" >&2
+                    exit 1
+                    ;;
+            esac
+            ;;
+        *)
+            echo "No self-compiled binary found and unsupported OS: $kernel" >&2
+            exit 1
+            ;;
+    esac
+}
+platform_file_end=$(get_platform_file_end)
+archive_file=$name-v$version-$platform_file_end
+
+github_url="https://github.com/chrisguida/$name/releases/download/v$version/$archive_file"
+
+
+# Download the archive using curl
+if ! curl -L "$github_url" -o "$script_dir/$archive_file"; then
+    echo "Error downloading the file from $github_url" >&2
+    exit 1
+fi
+
+# Extract the contents
+if [[ $archive_file == *.tar.gz ]]; then
+    if ! tar -xzvf "$script_dir/$archive_file" -C "$script_dir"; then
+        echo "Error extracting the contents of $archive_file" >&2
+        exit 1
+    fi
+elif [[ $archive_file == *.zip ]]; then
+    if ! unzip "$script_dir/$archive_file" -d "$script_dir"; then
+        echo "Error extracting the contents of $archive_file" >&2
+        exit 1
+    fi
+else
+    echo "Unknown archive format or unsupported file extension: $archive_file" >&2
+    exit 1
+fi
