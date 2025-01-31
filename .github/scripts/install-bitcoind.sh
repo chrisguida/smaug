b9dcdde6f6c8b7b0bfcf78c94e39b725a1ed05e4
Chris Guida <chrisguida@gmail.com>
2025-01-31 12:15:25 -0600

    suppress spammy wget progress output and update to bitcoind 28.1

--- .github/scripts/install-bitcoind.sh
+++ .github/scripts/install-bitcoind.sh
@@ -7,7 +7,7 @@ FILENAME="${DIRNAME}-x86_64-linux-gnu.tar.gz"
 
 cd /tmp/
 
-wget "https://bitcoincore.org/bin/bitcoin-core-${BITCOIN_VERSION}/${FILENAME}"
+wget -q "https://bitcoincore.org/bin/bitcoin-core-${BITCOIN_VERSION}/${FILENAME}"
 tar -xf "${FILENAME}"
 sudo mv "${DIRNAME}"/bin/* "/usr/local/bin"
 rm -rf "${FILENAME}" "${DIRNAME}"
